70922973e37cf490e62224712eff937169bd527f
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-03-26 16:58:28 +0600

    updated button functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,30 @@ class MyApp extends StatelessWidget {
             children: [
               FlatButton(onPressed: () async{
                 int i = await DatabaseHelper.instance.insert(
-                    {DatabaseHelper.comlumnName : "Sohag"}
+                    {DatabaseHelper.comlumnName : "Raiyan"}
                 );
                 print("id = $i");
+
               }, child: const Text("Insert",style: TextStyle(color: Colors.white),),color: Colors.blueGrey,),
+
+
               FlatButton(onPressed: () async{
                 List<Map<String,dynamic>> queryRows = await DatabaseHelper.instance.queryAll();
                 print("Query rows : $queryRows");
+
               }, child: const Text("Query",style: TextStyle(color: Colors.white),),color: Colors.green),
-              FlatButton(onPressed: (){}, child: const Text("Update",style: TextStyle(color: Colors.white),),color: Colors.lightBlue),
+
+
+              FlatButton(onPressed: () async{
+                int updatedId = await  DatabaseHelper.instance.update({
+                  DatabaseHelper.comlumnName: "Injamul",
+                  DatabaseHelper.columnId: 2
+                });
+                print("Updated Id : $updatedId");
+
+              }, child: const Text("Update",style: TextStyle(color: Colors.white),),color: Colors.lightBlue),
+
+
               FlatButton(onPressed: (){}, child: const Text("Delete",style: TextStyle(color: Colors.white),),color: Colors.blueAccent),
             ],
           ),
